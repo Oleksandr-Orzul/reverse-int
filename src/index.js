aa835bbd0ebd4450e5fc264e8db1ac3c755ff80c
Oleksandr <oleksandryurievich@gmail.com>
2023-01-12 12:23:32 +0200

    feat: create reverse func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = String(n);
+    str = str.replace("-", "");
+    const splitString = str.split("");
+    const reverseArray = splitString.reverse();
+    const joinArray = reverseArray.join("");
+    return Number(joinArray);
+};
